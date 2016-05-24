7ef7ce4af20fe6447632e2717fed1121d3c06d77
Alvaro <falconpower702@gmail.com>
2016-05-24 15:42:58 +0100

    Fixed fizzbuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,25 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+    public static void checkFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            isFizzBuzz(i);
+        }
+    }
+
+    private static void isFizzBuzz(int i) {
+        if (i % 3 == 0) {
+            System.out.println(i % 5 == 0 ? i + ": FizzBuzz" : i + ": Fizz");
+        } else if (i % 5 == 0) {
+            System.out.println(i % 3 == 0 ? i + ": FizzBuzz" : i + ": Buzz");
+        } else {
+            System.out.println(i);
+        }
+
+    }
+
     public static void main(String[] args) {
-        new FizzBuzzChecker().checkFizzBuzz(100);
+        checkFizzBuzz(100);
     }
 
 }
