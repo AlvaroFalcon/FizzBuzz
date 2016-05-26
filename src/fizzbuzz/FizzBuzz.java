bd0e14bc1a0b6e100b8cb5ad5e585d36a793ba0e
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-05-26 15:49:59 +0100

    Refactored code

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,36 +2,38 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
-    public static String fizzBuzzToString(int number, String fizz, String buzz) {
-        return number+": " + fizz + buzz + "\n";
-    }
-
-    public static String checkFizzBuzz(int max) {
+    public static String fizzBuzz(int max) {
         String fizzBuzzString = "";
         for (int number = 0; number <= max; number++) {
-            fizzBuzzString += isFizzBuzz(number);
+            fizzBuzzString += checkFizzBuzz(number);
         }
         return fizzBuzzString;
     }
 
-    private static String isFizzBuzz(int number) {
-        return fizzBuzzToString(number,isFizz(number),isBuzz(number));
+    private static String checkFizzBuzz(int number) {
+        return fizzBuzzToString(number, getFizz(number), getBuzz(number));
     }
 
-
-    private static String isFizz(int number) {
-        return isMult(number, 3) ? "Fizz" : "";
+    private static String getFizz(int number) {
+        if(isMult(number, 3)) return "Fizz";
+        return "";
     }
 
-    private static String isBuzz(int number) {
-        return isMult(number, 5) ? "Buzz" : "";
+    private static String getBuzz(int number) {
+        if(isMult(number,5)) return "Buzz";
+        return "";
     }
 
     private static boolean isMult(int number, int divisor) {
         return number % divisor == 0;
     }
+
+    public static String fizzBuzzToString(int number, String fizz, String buzz) {
+        return number + ": " + fizz + buzz + "\n";
+    }
+
     public static void main(String[] args) {
-        System.out.println(checkFizzBuzz(100));
+        System.out.println(fizzBuzz(100));
     }
 
 }
